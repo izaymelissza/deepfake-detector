ea5ff791bbdc67cd6437464938f9a6d59b542594
izaymelissza <melissaizaj@gmail.com>
2026-02-22 17:40:21 +0100

    deleting batchnorm1d

--- backend/model.py
+++ backend/model.py
@@ -9,7 +9,7 @@ class DeepfakeDetector(nn.Module):
         super(DeepfakeDetector, self).__init__()
         
         # Load EfficientNet-B4 backbone
-        self.backbone = models.efficientnet_b4(pretrained=False)
+        self.backbone = models.efficientnet_b4(weights=None)
         
         # Get number of features
         num_features = self.backbone.classifier[1].in_features
@@ -19,11 +19,9 @@ class DeepfakeDetector(nn.Module):
             nn.Dropout(p=0.4),
             nn.Linear(num_features, 512),
             nn.ReLU(),
-            nn.BatchNorm1d(512),
             nn.Dropout(p=0.3),
             nn.Linear(512, 256),
             nn.ReLU(),
-            nn.BatchNorm1d(256),
             nn.Dropout(p=0.2),
             nn.Linear(256, 1),
             nn.Sigmoid()
